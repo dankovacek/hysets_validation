1f8616c6190da0def30cebb3a5af8b8231d0181a
Dan K <dankovacek@gmail.com>
2022-02-28 16:52:19 -0800

    fix paths for cloud server

--- setup_scripts/process_hydrologic_regions.py
+++ setup_scripts/process_hydrologic_regions.py
@@ -10,8 +10,11 @@ import geopandas as gpd
 import shapely
 from shapely.geometry import Polygon
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(BASE_DIR, 'source_data/')
+# DATA_DIR = '/media/danbot/Samsung_T5/geospatial_data/'
 
-nhn_path = '../source_data/NHN_data/'
+nhn_path = os.path.join(DATA_DIR, 'NHN_data/')
 nhn_fname = 'NHN_INDEX_WORKUNIT_LIMIT_2.zip'
 if not os.path.exists(nhn_path + nhn_fname):
     print(f'   ...downloading national hydrologic network index file.')
@@ -25,11 +28,9 @@ if not os.path.exists(nhn_path + nhn_fname):
 nhn = gpd.read_file(nhn_path + nhn_fname)
 nhn = nhn.to_crs(3005)
 
-data_dir = '/media/danbot/Samsung_T5/geospatial_data/'
-
 # import BC border polygon
 
-bc_border_file = '../source_data/BC_border/BC_PROV_BOUNDARIES_LINES_500M.geojson'
+bc_border_file = os.path.join(DATA_DIR, 'BC_border/BC_PROV_BOUNDARIES_LINES_500M.geojson')
 bc_border_lines = gpd.read_file(bc_border_file)
 bc_border_crs = bc_border_lines.crs
 bc_border_polygon = Polygon(shapely.ops.linemerge(bc_border_lines.geometry.values))
@@ -57,8 +58,7 @@ def download_files(input):
 
 
 def download_hydrologic_feature_data(pgs):
-    save_directory = '/media/danbot/Samsung_T5/geospatial_data/WSC_data/NHN_feature_data/'
-    # save_directory = '../processed_data/WSC_data/NHN_feature_data/'
+    save_directory = os.path.join(DATA_DIR, 'NHN_feature_data/')
     if not os.path.exists(save_directory):
         os.mkdir(save_directory)
     
@@ -131,20 +131,24 @@ for k, v in groups.items():
     bc_merged_filled.loc[bc_merged_filled.index == k, 'WSCSDAs'] = ','.join(v)
 
 
-dem_dir = '../source_data/dem_data/'
-dem_dir = '/media/danbot/Samsung_T5/geospatial_data/DEM_data/'
+dem_dir = os.path.join(DATA_DIR, 'dem_data/')
 
 dem = rxr.open_rasterio(dem_dir + 'BC_DEM_mosaic_4326.vrt')
 dem_crs = dem.rio.crs.to_epsg()
 
 # save the output file
 bc_merged_filled = bc_merged_filled.to_crs(dem_crs)
-bc_merged_filled.to_file(f'../processed_data/merged_basin_groups/BC_basin_region_groups_EPSG{dem_crs}.geojson', driver='GeoJSON')
+output_folder = os.path.join(DATA_DIR, 'processed_data/merged_basin_groups/')
+if not os.path.exists(output_folder):
+    os.mkdir(output_folder)
+bc_merged_filled.to_file(output_folder + f'BC_basin_region_groups_EPSG{dem_crs}.geojson', driver='GeoJSON')
 print(f'   ...BC_basin_region_groups.geojson file created successfully.')
 
 # save each row as a separate shape file
-out_dir = '../processed_data/merged_basin_groups/split_groups/'
+split_out_dir = output_folder + 'split_groups/'
+if not os.path.exists(split_out_dir):
+    os.mkdir(split_out_dir)
 for code in bc_merged_filled.index.values:
     gdf = bc_merged_filled[bc_merged_filled.index == code].copy()
-    gdf.to_file(f'{out_dir}{code}_{dem_crs}.geojson', driver='GeoJSON')
-print(f'   ...saved individual polygons seperately at {out_dir}')
+    gdf.to_file(split_out_dir + f'{code}_{dem_crs}.geojson', driver='GeoJSON')
+print(f'   ...saved individual polygons seperately at {split_out_dir}')
