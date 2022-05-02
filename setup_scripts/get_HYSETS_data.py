9d16235a10f087ad18e5cd19f756f2a468240d2f
Dan K <dankovacek@gmail.com>
2022-05-02 10:23:46 -0700

    remove included files from HYSETS download script

--- setup_scripts/get_HYSETS_data.py
+++ setup_scripts/get_HYSETS_data.py
@@ -11,20 +11,20 @@ for p in [HYSETS_DIR, STN_DIR, BASIN_DIR]:
     if not os.path.exists(p):
         os.mkdir(p)
 
-filename = 'USGS_Streamgages-NHD_Locations_Shape.zip'
+# filename = 'USGS_Streamgages-NHD_Locations_Shape.zip'
 download_path = 'https://water.usgs.gov/GIS/dsdl/'
 
 # base_name = filename.split('.')[0]
 # download the file
-command = f'wget {download_path}{filename} -O {HYSETS_DIR}/{filename}'
-save_path = f'{STN_DIR}/{filename}'
-if not os.path.exists(save_path):
-    print(f'    ...downloading file {filename} from {download_path}')
-    os.system(command)
+# command = f'wget {download_path}{filename} -O {HYSETS_DIR}/{filename}'
+# save_path = f'{STN_DIR}/{filename}'
+# if not os.path.exists(save_path):
+#     print(f'    ...downloading file {filename} from {download_path}')
+#     os.system(command)
 
 # unzip the archive
-os.system(f'unzip {HYSETS_DIR}/{filename} -d {STN_DIR}')
-os.remove(f'{HYSETS_DIR}/{filename}')
+# os.system(f'unzip {HYSETS_DIR}/{filename} -d {STN_DIR}')
+# os.remove(f'{HYSETS_DIR}/{filename}')
 
 # get the very large file containing basin polygons
 download_path = 'https://files.osf.io/v1/resources/rpc3w/providers/googledrive/?zip='
@@ -32,7 +32,7 @@ basin_filename = 'HYSETS_watershed_boundaries.zip'
 command = f'wget {download_path}{basin_filename} -O {HYSETS_DIR}/{basin_filename}'
 save_path = f'{BASIN_DIR}/{basin_filename}'
 if not os.path.exists(save_path):
-    print(f'    ...downloading file {filename} from {download_path}')
+    print(f'    ...downloading file {basin_filename} from {download_path}')
     os.system(command)
 
 os.system(f'unzip {HYSETS_DIR}/{basin_filename} -d {BASIN_DIR}')
