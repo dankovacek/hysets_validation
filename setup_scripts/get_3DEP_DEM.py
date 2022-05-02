617d0cab2ab2e18a6613506245001e220a05a602
Dan K <dankovacek@gmail.com>
2022-05-02 11:30:09 -0700

    fix crs in 3DEP dem mosaic creation

--- setup_scripts/get_3DEP_DEM.py
+++ setup_scripts/get_3DEP_DEM.py
@@ -6,6 +6,8 @@ from multiprocessing import Pool
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEM_DIR = os.path.join(BASE_DIR, 'source_data/dem_data')
+DEM_DIR = os.path.join('/media/danbot/Samsung_T5/geospatial_data/DEM_data/')
+
 EP_DIR = os.path.join(DEM_DIR, 'USGS_3DEP')
 
 # ensure the folders exist
@@ -17,6 +19,7 @@ for p in [DEM_DIR, EP_DIR]:
 url_df = pd.read_csv(BASE_DIR + '/setup_scripts/file_lists/merged-files.txt', header=None)
 url_list = [e[0] for e in url_df.values if not os.path.exists(EP_DIR + '/' + e[0].split('/')[-1])]
 
+
 def download_file(url):
     command = f'wget {url} -P {EP_DIR}'
     os.system(command)
@@ -25,5 +28,5 @@ p = Pool()
 p.map(download_file, url_list)
 
 # # this command builds the dem mosaic "virtual raster"
-vrt_command = f"gdalbuildvrt -resolution highest -a_srs epsg:4326 {DEM_DIR}/BC_DEM_mosaic_4326.vrt {EP_DIR}/*.tif"
+vrt_command = f"gdalbuildvrt -resolution highest -a_srs epsg:4269 {DEM_DIR}/USGS_3DEP_mosaic_4269.vrt {EP_DIR}/*.tif"
 os.system(vrt_command)
